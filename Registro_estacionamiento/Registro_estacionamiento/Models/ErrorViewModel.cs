79d9a7632fb4e8a156027b728345a0ba857cde80
maquirsarao <maquirsarao@gmail.com>
2024-07-09 21:53:58 -0600

    primer commit

---
+++ Registro_estacionamiento/Registro_estacionamiento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registro_estacionamiento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
